e85750ed0a77546ee9342dd0bbaf174af768dfd7
Simon Auber <simon.auber@hetic.net>
2016-05-16 23:48:16 +0200

    Create functions

--- src/sigmoid.c
+++ src/sigmoid.c
@@ -1,1 +1,14 @@
-// Being developped...
+#include <math.h>
+#include "sigmoid.h"
+
+double sigmoid(double n) {
+    return (1 / (1 + pow(EULER_NUMBER, -n)));
+}
+
+float sigmoidf(float n) {
+    return (1 / (1 + powf(EULER_NUMBER_F, -n)));
+}
+
+long double sigmoidl(long double n) {
+    return (1 / (1 + powl(EULER_NUMBER_L, -n)));
+}
